3aeffec2296e7b177c2d5395d7f981137bee62d7
Abigail Watson <abigailwatson@Abigails-Mac-mini-379644.local>
2018-09-09 02:02:19 -0500

    QA pass

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-subscription',
-  version: '3.5.5',
+  version: '3.5.6',
   summary: 'HL7 FHIR Resource - Subscription',
   git: 'https://github.com/clinical-meteor/hl7-resource-subscription',
   documentation: 'README.md'
@@ -28,7 +28,7 @@ Package.onUse(function (api) {
   api.use('clinical:glass-ui@2.2.1');
   api.use('clinical:extended-api@2.2.2');
   api.use('clinical:base-model@1.4.0');
-  api.use('clinical:user-model@1.5.0');
+  api.use('clinical:user-model@1.6.2');
   api.use('matb33:collection-hooks@0.7.15');
   api.use('clinical:alert@2.1.3');
   
