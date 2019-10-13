92453ccc7ce7b9b96a8dc5f45fe93cfbc0d94234
Abigail Watson <awatson1978@gmail.com>
2019-10-13 15:51:29 -0500

    bump glass-ui dep

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-subscription',
-  version: '3.5.15',
+  version: '3.5.16',
   summary: 'HL7 FHIR Resource - Subscription',
   git: 'https://github.com/clinical-meteor/hl7-resource-subscription',
   documentation: 'README.md'
@@ -24,7 +24,7 @@ Package.onUse(function (api) {
   api.use('http');
   api.use('react-meteor-data@0.2.15');
 
-  api.use('clinical:glass-ui@2.2.1');
+  api.use('clinical:glass-ui@2.4.7');
   api.use('clinical:extended-api@2.2.2');
   api.use('clinical:base-model@1.4.0');
   api.use('clinical:user-model@1.6.2');
